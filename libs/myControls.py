e0d5cc76fac8080aae929de47c2b12841e2186ed
IrvingHdez <irving.hernandez.g@gmail.com>
2018-12-06 17:48:29 +0100

    creating class

--- libs/myControls.py
+++ libs/myControls.py
@@ -7,37 +7,38 @@
 import time, threading
 import ipywidgets as widgets
 
-# btn - receive a button object
-# lbl - receive a label object
-#    we use lbl.value to get timer value
-# q - receive a multiple option object
-#    we use q.value to get answered value
-def timer(btn,lbl,q):        
+class myControls:
+    # btn - receive a button object
+    # lbl - receive a label object
+    #    we use lbl.value to get timer value
+    # q - receive a multiple option object
+    #    we use q.value to get answered value
+    def timer(btn,lbl,q):        
+            
+        cnt =int(lbl.value)   
+        on=btn.value 
+        desc=q1.description
+
+        if desc!='Submitted':
+            if on==True:      
+                threading.Timer(1, timer, [btn,lbl,q]).start()
+                cnt = cnt+1                        
+                lbl.value = str(cnt)
+                btn.description='Submit answer'
+                q.disabled=False
+
+            elif on==False:
+                threading.Timer(1, timer, [btn,lbl,q]).start()
+                btn.description='Activate'
+                q.disabled=True
+
+            else:
+                None
         
-    cnt =int(lbl.value)   
-    on=btn.value 
-    desc=q1.description
-
-    if desc!='Submitted':
-        if on==True:      
+        if cnt!=0 and btn.description=='Activate':
             threading.Timer(1, timer, [btn,lbl,q]).start()
-            cnt = cnt+1                        
-            lbl.value = str(cnt)
-            btn.description='Submit answer'
-            q.disabled=False
-
-        elif on==False:
-            threading.Timer(1, timer, [btn,lbl,q]).start()
-            btn.description='Activate'
-            q.disabled=True
-
-        else:
-            None
-    
-    if cnt!=0 and btn.description=='Activate':
-        threading.Timer(1, timer, [btn,lbl,q]).start()
-        q1.description='Submitted'
-        btn.visibility='hidden'
-        btn.description='--'
-        btn.disbled=True    
+            q1.description='Submitted'
+            btn.visibility='hidden'
+            btn.description='--'
+            btn.disbled=True    
 
